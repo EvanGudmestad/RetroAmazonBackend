f6f2b7c6d295d1137d05a6451021edb2c199bf5e
EvanGudmestad <eagudmestad@ranken.edu>
2023-10-17 08:16:05 -0500

    Added back end validation with JOI

--- routes/api/book.js
+++ routes/api/book.js
@@ -2,8 +2,31 @@ import express from 'express';
 import debug from 'debug';
 const debugBook = debug('app:Book');
 import { connect,getBooks, getBookById, updateBook, addBook, deleteBook } from '../../database.js';
+import { validId } from '../../middleware/validId.js';
+import {validBody} from '../../middleware/validBody.js';
+import Joi from 'joi';
 
 const router = express.Router();
+
+const newBookSchema = Joi.object({
+    isbn:Joi.string().trim().min(14).required(),
+    title:Joi.string().trim().min(1).required(),
+    author:Joi.string().trim().min(1).required(),
+    genre:Joi.string().valid('Fiction', 'Magical Realism', 'Dystopian', 'Mystery', 'Young Adult', 'Non-Fiction').required(),
+    publication_year:Joi.number().integer().min(1900).max(2023).required(),
+    price:Joi.number().min(0).required(),
+    description:Joi.string().trim().min(1).required(),
+});
+
+const updateBookSchema = Joi.object({
+    isbn:Joi.string().trim().min(14),
+    title:Joi.string().trim().min(1),
+    author:Joi.string().trim().min(1),
+    genre:Joi.string().valid('Fiction', 'Magical Realism', 'Dystopian', 'Mystery', 'Young Adult', 'Non-Fiction'),
+    publication_year:Joi.number().integer().min(1900).max(2023),
+    price:Joi.number().min(0),
+    description:Joi.string().trim().min(1),
+});
 
 //get all books
 router.get('/list', async (req, res) => {
@@ -21,11 +44,15 @@ router.get('/list', async (req, res) => {
 });
 
 //get a book by the id
-router.get('/:id', async (req,res) => {
-    const id = req.params.id;
+router.get('/:id', validId('id'), async (req,res) => {
+    const id = req.id;
     try{
         const book = await getBookById(id);
-        res.status(200).json(book);
+        if(book){
+            res.status(200).json(book);
+        }else{
+            res.status(404).json({message: `Book ${id} not found`});
+        }
     } catch(err){
         res.status(500).json({error: err.stack});
     }
@@ -33,8 +60,8 @@ router.get('/:id', async (req,res) => {
 
 //update a book by the id
 //update can use a put or a post
-router.put('/update/:id', async (req,res) => {
-    const id = req.params.id;
+router.put('/update/:id', validId('id'), validBody(updateBookSchema), async (req,res) => {
+    const id = req.id;
     const updatedBook = req.body;
     if(updatedBook.price){
         updatedBook.price = parseFloat(updatedBook.price);
@@ -53,7 +80,7 @@ router.put('/update/:id', async (req,res) => {
 
 
 //add a new book to the Mongo Atlas database
-router.post('/add', async (req,res) => {
+router.post('/add', validBody(newBookSchema), async (req,res) => {
     //req is the request object
     const newBook = req.body;
    
@@ -70,9 +97,9 @@ router.post('/add', async (req,res) => {
 });
 
 //delete a book by the id
-router.delete('/delete/:id', async (req,res) => {
+router.delete('/delete/:bookId', validId('bookId'), async (req,res) => {
     //gets the id from the URL
-    const id = req.params.id;
+    const id = req.bookId;
 
     try{
         const dbResult = await deleteBook(id);
--- routes/api/user.js
+++ routes/api/user.js
@@ -4,26 +4,41 @@ const debugUser = debug('app:User');
 debugUser.color = '63';
 import {addUser, loginUser} from '../../database.js';
 import bcrypt from 'bcrypt';
+import { validBody } from '../../middleware/validBody.js';
+import Joi from 'joi';
 
 const router = express.Router();
+
+//step 1: define new user schema
+const newUserSchema = Joi.object({
+    fullName:Joi.string().trim().min(1).max(50).required(),
+    password:Joi.string().trim().min(8).max(50).required(),
+    email:Joi.string().trim().email().required(),
+});
+
+const loginUserSchema = Joi.object({
+    email:Joi.string().trim().email().required(),
+    password:Joi.string().trim().min(8).max(50).required()
+});
 
 router.get('/list', (req, res) => {
     debugUser('Getting all users');
     res.send('Hello From Amazon.com User Route!');
 });
 
-router.post('/add', async (req,res) => {
+router.post('/add', validBody(newUserSchema), async (req,res) => {
     const newUser = req.body;
     newUser.password = await bcrypt.hash(newUser.password, 10);
     try{
         const result = await addUser(newUser);
+      
         res.status(200).json({message: `User ${result.insertedId} added`});
     }catch(err){
         res.status(500).json({error: err.stack});
     }
 });
 
-router.post('/login', async (req,res) => {
+router.post('/login', validBody(loginUserSchema), async (req,res) => {
     const user = req.body;
 
     const resultUser = await loginUser(user);
